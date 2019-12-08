1581696b985f4d38e859d3f7ec25562596a8fcb4
Megan Nowak <MNowak16@my.wctc.edu>
2019-12-08 15:16:03 -0600

    Set up 7-9 Menu

---
+++ Northwind-Console/Utils/ProductUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindConsole.Utils
+{
+    class ProductUtils
+    {
+        public static void DisplayMenu()
+        {
+
+        }
+
+        public static void Add()
+        {
+
+        }
+
+        public static void Edit()
+        {
+
+        }
+    }
+}
